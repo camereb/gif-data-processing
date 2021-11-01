034241a1d84e2812250b45f88264fa32c0ac501c
MaiaPelletier <maia.pelletier15@gmail.com>
2021-11-01 10:32:20 -0400

    add sources to indicators that already have them

--- metadata-populating.R
+++ metadata-populating.R
@@ -8,19 +8,26 @@ library(purrr)
 
 extract_en_metadata <- function(yml_file) {
   
-  file_name <- yml_file %>% str_extract("[0-9]*-[0-9a-z]*-[0-9a-z]*\\.yml$")
+  # extract indicator number from file path
+  file_name <- yml_file
   
+  # get path to file
+  yml_file <- here("metadata", "translations-metadata", "en", yml_file)
+  
+  # read yml
   raw_meta <- read_yaml(yml_file)
   
+  # extract definition from definition and concepts (WIP)
   definition_field <- unlist(str_split(raw_meta$STAT_CONC_DEF, "\n"))
   definition_position <- which(str_detect(definition_field, "Definition:")) + 1
   raw_meta$STAT_CONC_DEF  <- definition_field[definition_position]
   
+  # create global metadata fields
   raw_meta$SDG_GOAL__GLOBAL <- raw_meta$SDG_GOAL
   raw_meta$SDG_TARGET__GLOBAL <- raw_meta$SDG_TARGET
   raw_meta$SDG_INDICATOR__GLOBAL <- raw_meta$SDG_INDICATOR
   
-  
+  # fields to take from the raw_meta data
   fields <- c(
     "SDG_GOAL", 
     "SDG_TARGET",
@@ -31,14 +38,60 @@ extract_en_metadata <- function(yml_file) {
     "SDG_INDICATOR__GLOBAL"
   )
   
-  write_yaml(raw_meta[fields], here("metadata", "meta", file_name))
   return(raw_meta[fields])
   
 }
 
 
-all_metadata_files <- list.files(here("metadata", "translations-metadata", "en"), full.names = TRUE)
+# Extract sources from gcdocs metadata --------------------------------
+
+extract_source_metadata <- function(source_file) {
+  
+  source_file <- paste0("indicator_", str_replace(source_file, ".yml", ".txt"))
+  source_file <- here("metadata", "gcdocs", source_file)
+  
+  if (!file.exists(source_file)) {
+    
+    return(NULL)
+    
+  } else {
+    
+    # read metadata files (text files)
+    gcdocs_md <- readLines(source_file) 
+    
+    # filter for source fields
+    fields <- gcdocs_md[which(str_detect(gcdocs_md, "source"))]
+    
+    # format for yaml file
+    field_names <- str_remove(str_extract(fields, "^\\w*:"), ":")
+    fields <- trimws(str_remove(fields, "^\\w*:"))
+    names(fields) <- field_names
+    source_metadata <- as.list(fields)
+    
+    source_metadata$reporting_status <- "complete"
+    source_metadata$COVERAGE <- source_metadata$source_geographical_coverage_1
+  
+    return(source_metadata)
+    #write_yaml(source_metadata, "test_source.yml")
+    
+  }
+  
+}
+
+write_new_metadata <- function(file) {
+  
+  sdg_md <- extract_en_metadata(file)
+  source_md <- extract_source_metadata(file)
+  all_metadata <- c(sdg_md, source_md)
+  
+  # write the new yml for the new metadata
+  write_yaml(all_metadata, here("metadata", "meta", file))
+  
+}
+
+all_metadata_files <- list.files(here("metadata", "translations-metadata", "en"))
 # temp until can figure out 5-4-1 fix
-all_metadata_files <- all_metadata_files[!str_ends(all_metadata_files, "/5-4-1.yml")]
-all_metadata <- map(all_metadata_files, extract_en_metadata)
+all_metadata_files <- all_metadata_files[!str_ends(all_metadata_files, "5-4-1.yml")]
+all_metadata <- map(all_metadata_files, write_new_metadata)
+
 
