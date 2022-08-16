ce1e5a15637e3c7e048b68e0e2cd259e9096bf3c
Grewal <keerti.grewal@statcan.gc.ca>
2022-08-16 12:21:54 -0400

    Update 3-6-1 and 3-7-1

--- CIF/R/indicator_3-7-1.R
+++ CIF/R/indicator_3-7-1.R
@@ -1,8 +1,7 @@
 
-
 #13-10-0096-01
 
-# CIF 3.6.1 ---------------------------------------------------------------
+# CIF 3.7.1 ---------------------------------------------------------------
 
 # load libraries
 library(dplyr)
@@ -16,46 +15,40 @@ Raw_data <- get_cansim("13-10-0096-01", factors = FALSE)
 # load geocode
 geocodes <- read_csv("geocodes.csv")
 
+View(Raw_data)
 
-overall_health <- 
+mental_health <- 
   Raw_data %>% 
-  filter(Indicators == "Perceived health, very good or excellent", 
-         Characteristics == "Percent") %>% 
+  filter(Characteristics == "Percent", 
+         Indicators == "Perceived mental health, very good or excellent") %>% 
   select(REF_DATE, GEO, `Age group`, Sex, VALUE) %>% 
   rename(Year = REF_DATE, Geography = GEO, Value = VALUE) %>% 
   left_join(geocodes, by = "Geography") %>% 
   relocate(GeoCode, .before = Value)
 
-
 total <- 
-  overall_health %>% 
-  filter(Geography == "Canada (excluding territories)", 
+  mental_health %>% 
+  filter(Geography == "Canada (excluding territories)",
          `Age group` == "Total, 12 years and over",
          Sex == "Both sexes") %>% 
   mutate_at(2:(ncol(.)-2), ~ "")
 
+
 non_total <- 
-  overall_health %>% 
-  filter(!(Geography == "Canada (excluding territories)" &
-          `Age group` == "Total, 12 years and over" &
-          Sex == "Both sexes")) %>% 
+  mental_health %>% 
+  filter(!(Geography == "Canada (excluding territories)"&
+           `Age group` == "Total, 12 years and over" &
+           Sex == "Both sexes")) %>% 
   mutate_at(2:(ncol(.)-2), ~ paste0("data.", .x))
 
-final_data <- 
+
+final_data <-
   bind_rows(total, non_total)
 
-names(final_data)[2:(ncol(final_data)-2)] <- paste0("data." ,
+names(final_data)[2:(ncol(final_data)-2)] <- paste0("data.", 
                                                     names(final_data)[2:(ncol(final_data)-2)])
 
-write_csv(final_data, "CIF/data/indicator_3-6-1.csv", na = "")
-  
-  
-  
-  
-  
-  
-  
-  
+write_csv(final_data, "CIF/data/indicator_3-7-1.csv", na = "")
 
 
 
