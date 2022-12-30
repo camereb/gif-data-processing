564d2a9c290d4959dab179c102771c195b091bcb
Grewal <keerti.grewal@statcan.gc.ca>
2022-12-30 16:38:14 -0500

    Add indicator 16-4-1

---
+++ CIF/R/indicator_16-4-1.R
@@ -0,0 +1,93 @@
+
+
+#35-10-0001-01 
+
+# CIF 16.3.1 --------------------------------------------------------------------
+
+#load libraries 
+library(dplyr)
+library(readr)
+library(tidyr)
+library(cansim)
+library(stringr)
+
+
+
+
+Raw_data <- get_cansim("35-10-0001-01", factors = FALSE)
+
+
+
+cyber_violation <- c(
+  "Total, all violations",
+  "Luring a child via a computer",
+  "Non-consensual distribution of intimate images",
+  "Extortion",
+  "Criminal harassment",
+  "Indecent/Harassing communications",
+  "Uttering threats",
+  "Fraud",
+  "Identity theft",
+  "Identity fraud",
+  "Child pornography",
+  "Making or distribution of child pornography"
+)
+
+
+cyber_crime <- 
+  Raw_data %>% 
+  filter(REF_DATE >= 2015,
+         `Cyber-related violation` %in% cyber_violation) %>% 
+  select(Year = REF_DATE,
+         Geography = GEO,
+         `Cyber-related violation`,
+         Value = VALUE) %>% 
+  mutate(Geography = recode(Geography, 
+                            "Canada, selected police services" = "Canada")) %>% 
+  mutate(`Type of cyber-related violation` = "Against a person") %>% 
+  relocate(`Type of cyber-related violation`, .after = Geography)
+
+
+#Create the total and non-total line 
+
+total <- 
+  cyber_crime %>% 
+  filter(Geography == "Canada",
+         `Cyber-related violation` == "Total, all violations") %>% 
+  mutate_at(2:(ncol(.)-1), ~ "")
+
+
+
+non_total <- 
+  cyber_crime %>% 
+  filter(!(Geography == "Canada" &
+           `Cyber-related violation` == "Total, all violations")) %>% 
+  mutate_at(2:(ncol(final_data)-1), ~ paste0("data.", .x))
+
+
+#Format final table and export to csv 
+
+
+final_data <- 
+  rbind(total, non_total)
+
+
+names(final_data)[2:(ncol(final_data)-1)] <- 
+  paste0("data.", names(final_data)[2:(ncol(final_data)-1)])
+
+
+write_csv(final_data, "CIF/data/indicator_16-4-1.csv", na = "")
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
